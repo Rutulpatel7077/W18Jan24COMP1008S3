c51be41c467ef30c1a108792150729a2d7cba017
JaretWright <hkkA223>
2018-01-24 13:10:14 -0500

    Added functionality to the Card class

--- src/w18comp1008s3jan24/Card.java
+++ src/w18comp1008s3jan24/Card.java
@@ -11,6 +11,10 @@ public class Card {
     //these are "instance" variables.  We make them private so that our
     //class controls how they are modified and accessed
     private String faceName, suit;
+    private static final String[] validSuits = 
+                                        {"spades","hearts","diamonds","clubs"};
+    private static final String[] validFaceNames = 
+            {"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
 
     /**
      * This is a constructor.  It is automatically called when we create 
@@ -21,7 +25,40 @@ public class Card {
      * @param suit 
      */
     public Card(String faceName, String suit) {
-        this.faceName = faceName;
+        setFaceName(faceName);
+        setSuit(suit);
+    }
+
+    public String getFaceName() {
+        return faceName;
+    }
+
+    /**
+     * This method validates that the facename is defined in the
+     * faceName array
+     * @param faceName 
+     */
+    public void setFaceName(String faceName) {
+        for (String validFaceName : validFaceNames)
+        {
+            if (validFaceName.equalsIgnoreCase(faceName)){
+                this.faceName=validFaceName;
+                return;
+            }
+        }
+        //if none of the names matched, we made it to this line of 
+        //code, so we should throw an exception
+        throw new IllegalArgumentException("Face names must be 2-10, Jack, Queen"
+                + "King or Ace");
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
         this.suit = suit;
     }
+    
+    
 }
